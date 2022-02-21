750095b3a12cc63a719dac4fc90d4f36a61f75c2
tolgaakn <akintolga@outlook.com>
2022-02-21 22:27:23 +0300

    Added result structure.

---
+++ carrental/src/main/java/myproject/carrental/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package myproject.carrental.core.utilities.results;
+
+public class SuccessResult extends Result{
+
+	public SuccessResult() {
+		super(true);
+	}
+	
+	public SuccessResult(String message) {
+		super(true, message);
+	}
+}
